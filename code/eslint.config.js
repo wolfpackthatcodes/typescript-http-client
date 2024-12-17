98b062c23de19bd5d03077bda8df2f291aade650
Luis Carlos Aveiro <git@luisaveiro.com>
2024-12-17 21:04:22 +0000

    build: update eslint & prettier configs

---
+++ code/eslint.config.js
@@ -0,0 +1,25 @@
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  {
+    ...importPlugin.flatConfigs.recommended,
+    ...importPlugin.flatConfigs.typescript,
+  },
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+];
